3efa06c9abf6d1218156e16a28dd43c95aa86ec1
ProfesserVarXX <101981395+ProfesserVarXX@users.noreply.github.com>
2022-03-28 18:11:11 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 let passengers = document.querySelector(".person");
+//let element = document.createElement("h1");
 let defaults = 0;
 
 passengers.innerHTML = "Total: " + defaults;
-document.body.appendChild(passengers);
+//document.body.appendChild(passengers);
 
 function increment() {
     let increments = defaults += 1;
     passengers.innerHTML = "Total: " + increments;
-    document.body.appendChild(passengers)
+    //document.body.appendChild(passengers)
 }
