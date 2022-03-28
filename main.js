4906cb99f56323cf63250e06f1118d330c209b6a
ProfesserVarXX <101981395+ProfesserVarXX@users.noreply.github.com>
2022-03-28 17:55:11 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const passengers = document.querySelector(".people");
+let passengers = document.querySelector(".people");
 let defaults = 0;
 
-passengers.innerHTML = "Total: " + defaults;
+passengers.innerText = "Total: " + defaults;
 
 function increment() {
     let increments = defaults += 1;
-    passengers.innerHTML = "Total: " + increments;
+    passengers.innerText = "Total: " + increments;
 }
